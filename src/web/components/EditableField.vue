d382026f2c1aeedb325ee71504d7e4342df395a5
Arjit Srivastava <srivastavarjit@gmail.com>
2020-09-12 23:21:24 +0530

    [Web] Improved editable field support

--- src/web/components/EditableField.vue
+++ src/web/components/EditableField.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div ref="editableField" class="editable-container" @click="edit">
+    <div ref="editableField" :class="editableClass" @click="edit">
       <slot></slot>
     </div>
     <div v-if="singleLine">
@@ -9,7 +9,12 @@
         ref="editField"
         v-model="model"
         label="Edit"
-        @blur="finish"
+        counter="80"
+        hint="Press Ctrl + Enter to save"
+        clearable
+        @blur="cancel"
+        @click:clear="cancel"
+        @keydown="waitForFinish($event)"
       >
       </v-text-field>
     </div>
@@ -19,7 +24,11 @@
         ref="editField"
         v-model="model"
         label="Edit"
-        @blur="finish"
+        clearable
+        hint="Press Ctrl + Enter to save"
+        @blur="cancel"
+        @click:clear="cancel"
+        @keydown="waitForFinish($event)"
       ></v-textarea>
     </div>
   </div>
@@ -27,6 +36,10 @@
 <style scoped>
 .editable-container {
   position: relative;
+}
+
+.disabled {
+  pointer-events: none !important;
 }
 
 .editable-container:hover {
@@ -45,21 +58,52 @@ export default {
       type: String,
       default: '',
     },
+    editable: {
+      type: Boolean,
+      default: false,
+    },
   },
   data: () => ({
     isEditing: false,
+    bModel: '',
   }),
+  computed: {
+    editableClass() {
+      return this.editable ? 'editable-container' : 'disabled'
+    },
+  },
   methods: {
+    waitForFinish(e) {
+      if (e.which === 13 && e.ctrlKey) {
+        this.finish()
+      }
+    },
+
+    cancel() {
+      this.isEditing = false
+      this.model = this.bModel
+      this.finish()
+    },
+
     finish() {
       const firstChild = this.$refs.editableField.firstChild
       firstChild.style.display = ''
+
+      if (!this.isEditing) {
+        return
+      }
+
       this.isEditing = false
+      this.bModel = this.model // Update backup property
+      this.$emit('update:model', this.model)
     },
 
     edit() {
       const firstChild = this.$refs.editableField.firstChild
       firstChild.style.display = 'none'
       this.isEditing = true
+      this.bModel = this.model
+
       const textarea = this.$refs.editField.$el.querySelector('textarea,input')
       this.$nextTick(() => {
         textarea.focus()
